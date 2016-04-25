40c569bb927bd0aab47587b874aafa63b5c22283
Jani Hur <webmaster@jani-hur.net>
2016-04-25 20:58:51 +0300

    Passwordless sudo.

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -5,3 +5,7 @@ wget --no-check-certificate -O authorized_keys 'https://github.com/mitchellh/vag
 mv authorized_keys /home/vagrant/.ssh
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
+
+echo "==> Passwordless sudo for ${SSH_USERNAME}"
+echo "${SSH_USERNAME}        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
+chmod 440 /etc/sudoers.d/vagrant
