274086f9898db2a75f1a7380af65eb96f3e6ca2c
Jani Hur <webmaster@jani-hur.net>
2016-05-14 20:44:22 +0300

    Default ssh key included and not downloaded from net during build.

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-mkdir /home/vagrant/.ssh
-wget --no-check-certificate -O authorized_keys 'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub'
-mv authorized_keys /home/vagrant/.ssh
-chown -R vagrant /home/vagrant/.ssh
-chmod -R go-rwsx /home/vagrant/.ssh
+SSH_USER_HOME="/home/${SSH_USERNAME}"
+
+echo "==> Default SSH key"
+mkdir ${SSH_USER_HOME}/.ssh
+chmod 700 ${SSH_USER_HOME}/.ssh
+
+# https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+VAGRANT_INSECURE_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+
+echo "${VAGRANT_INSECURE_KEY}" > ${SSH_USER_HOME}/.ssh/authorized_keys
+chmod 600 ${SSH_USER_HOME}/.ssh/authorized_keys
+chown -R ${SSH_USERNAME}:${SSH_USERNAME} ${SSH_USER_HOME}/.ssh
 
 echo "==> Passwordless sudo for ${SSH_USERNAME}"
 echo "${SSH_USERNAME}        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
