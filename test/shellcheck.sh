e904a071f3d7ca646497b4143ecb850a2052b6ea
capusta <gvozdev@gmail.com>
2017-01-27 10:49:06 -0800

    What exactly is travis executing?

--- test/shellcheck.sh
+++ test/shellcheck.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+set -x
 case "$(lsb_release -c | cut -f 2)" in
     xenial|trusty|precise)
         switch=$(dpkg -s shellcheck | grep -i version | cut -f 2 -d ' ' | grep '0.4' && echo '-x')
