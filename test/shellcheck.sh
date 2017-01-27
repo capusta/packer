d7c7212da4011e52017afdd0ce14f3ad15135fbd
capusta <gvozdev@gmail.com>
2017-01-27 10:56:29 -0800

    stupid travis

--- test/shellcheck.sh
+++ test/shellcheck.sh
@@ -2,7 +2,8 @@
 set -x
 case "$(lsb_release -c | cut -f 2)" in
     xenial|trusty|precise)
-        switch=$(dpkg -s shellcheck | grep -q "^0.4" && echo '-x')
+        dpkg -s shellcheck
+        switch=$(dpkg -s shellcheck | grep -q "0.4" && echo '-x')
         bash -c "shellcheck ${switch} bin/build.sh"
         ;;
     CentOS)
