4b14abef0592a7bd0102481564fb416e7259ee4e
capusta <capusta@users.noreply.github.com>
2017-06-01 02:00:06 -0700

    fixing directories

--- windows/test.pp
+++ windows/test.pp
@@ -1,7 +1,15 @@
 include chocolatey
 Package { provider => chocolatey, }
 
-file { ['c:\var','c:\var\consul']:
+$working_dirs = [
+	'c:\var',
+	'c:\var\consul',
+	'c:\var\consul-template\'
+	'c:\var\consul-template\config'
+	'c:\var\consul-template\templates' 
+	]
+	
+file { $working_dirs:
 	ensure => directory
 }
 package { ['nssm','consul','consul-template']:
