bdf572701e71a531657d684a398faf91ded90de4
Capusta <capusta@users.noreply.github.com>
2017-09-19 10:22:29 -0700

    Role cleanup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-echo 'I am empty ... but will run sometime on the box being built'
+
+# Install ansible for further provisioning
+apt-get install software-properties-common && \
+apt-add-repository ppa:ansible/ansible && \
+apt-get update && \
+apt-get -y install ansible python-pip
+pip install docker-py python-consul
