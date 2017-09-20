b9a33affc1131715d27a85bda149fdf4a79e61fc
Capusta <capusta@users.noreply.github.com>
2017-09-20 13:42:53 -0700

    Remove ansible provisioning in the build phase ..
    this will be called with "vagrant provision" and can set the playbook in env variable instead.
    This allows you to garget specific playbook filenames as long as they are uniqie.  if not, the first playbook that matches
    will be applied.  extra config options (connections, extra vars, etc) can also be injected via env variables.  more documentation
    will come later

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -eux
 
+# Not sure if we are going to run this or not
+#dpkg --list | awk '{ print $2 }' | grep 'linux-image-4.*-generic' | grep -v $(uname -r) | xargs apt-get -y purge
+
+apt-get -y autoremove --purge
+apt-get -y autoclean
+apt-get -y clean
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=16M || echo "dd exit code $? suppressed";
